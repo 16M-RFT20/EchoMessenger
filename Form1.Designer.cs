c4fa1e34e6be70f6b460e6061ef343c8a7d008ef
kim dg <dgkim729@gmail.com>
2026-03-19 11:12:58 +0900

    과제1 코딩과 테스트 완료 ListBox를 중앙에 TextBox를 ListBox 아래에 배치함 전송 버튼을 누르면 TextBox에 입력한 내용이 ListBox로 들어가 표시되고 TextBox의 내용은 깨끗하게 지워짐

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            lblEchoMessenger = new Label();
+            myTextBox = new TextBox();
+            mylistBox = new ListBox();
+            btnTransmit = new Button();
+            SuspendLayout();
+            // 
+            // lblEchoMessenger
+            // 
+            lblEchoMessenger.AutoSize = true;
+            lblEchoMessenger.Font = new Font("한컴 고딕", 25.8F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            lblEchoMessenger.ForeColor = Color.Blue;
+            lblEchoMessenger.Location = new Point(27, 22);
+            lblEchoMessenger.Name = "lblEchoMessenger";
+            lblEchoMessenger.Size = new Size(348, 56);
+            lblEchoMessenger.TabIndex = 0;
+            lblEchoMessenger.Text = "Echo Messenger";
+            // 
+            // myTextBox
+            // 
+            myTextBox.Font = new Font("한컴 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            myTextBox.Location = new Point(27, 375);
+            myTextBox.Name = "myTextBox";
+            myTextBox.Size = new Size(588, 43);
+            myTextBox.TabIndex = 1;
+            // 
+            // mylistBox
+            // 
+            mylistBox.Font = new Font("한컴 고딕", 19.7999973F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            mylistBox.FormattingEnabled = true;
+            mylistBox.Location = new Point(27, 81);
+            mylistBox.Name = "mylistBox";
+            mylistBox.Size = new Size(738, 262);
+            mylistBox.TabIndex = 2;
+            // 
+            // btnTransmit
+            // 
+            btnTransmit.BackColor = Color.FromArgb(128, 255, 128);
+            btnTransmit.Font = new Font("한컴 고딕", 18F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnTransmit.Location = new Point(621, 363);
+            btnTransmit.Name = "btnTransmit";
+            btnTransmit.Size = new Size(144, 75);
+            btnTransmit.TabIndex = 3;
+            btnTransmit.Text = "전송";
+            btnTransmit.UseVisualStyleBackColor = false;
+            btnTransmit.Click += btnTransmit_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 255, 192);
             ClientSize = new Size(800, 450);
+            Controls.Add(btnTransmit);
+            Controls.Add(mylistBox);
+            Controls.Add(myTextBox);
+            Controls.Add(lblEchoMessenger);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblEchoMessenger;
+        private TextBox myTextBox;
+        private ListBox mylistBox;
+        private Button btnTransmit;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,16 @@ namespace EchoMessenger
         {
             InitializeComponent();
         }
+
+        private void btnTransmit_Click(object sender, EventArgs e)
+        {
+            string typed_msg;
+            typed_msg = myTextBox.Text;
+            mylistBox.Items.Add(typed_msg);
+            // TextBox에 입력된 내용을 ListBox에 추가
+
+            myTextBox.Clear();
+            // TextBox의 내용을 지움
+        }
     }
 }
